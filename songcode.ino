bb009b855fcfba316d8af9e778e901645b7fd2a6
ajbobrovsky <ajbobrovsky@yahoo.com>
2023-03-29 22:03:12 -0500

    testing

--- songcode.ino
+++ songcode.ino
@@ -2,9 +2,14 @@ void setup()
 {
   // Andrew code starts
   // I'm adding a new line
+  //testing vs1
+int tempo = 60000/113; //second value is desired tempo (can be changed)
+
 }
 void loop()
 {
+  for (int i = 0; i < 3; i++){
+  }
   // Fabian changes here
   // I'm adding another line
 }
